056dcd42b7a41b18f6d00860049d88fbc1d6c21b
eeeeoino <eetu.oinonen04@gmail.com>
2025-09-08 15:21:26 +0300

    changes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+const Note = require('./models/note')
 
 let notes = [
   {
@@ -29,7 +28,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -77,7 +78,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
